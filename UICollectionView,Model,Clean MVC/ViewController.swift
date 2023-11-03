ee2033d6874fe0da4073f03b1250448264ee2584
iOSipovDobrynia <dobrynia.osipov@gmail.com>
2023-11-03 21:25:52 +0300

    Initial Commit

---
+++ UICollectionView,Model,Clean MVC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UICollectionView,Model,Clean MVC
+//
+//  Created by Goodwasp on 03.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
